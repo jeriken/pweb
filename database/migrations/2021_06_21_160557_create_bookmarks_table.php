30c664eab0cc1c6fea4233e0180735bf47dbd1fa
ridhayani76@gmail.com <ridhayani76@gmail.com>
2021-06-22 00:22:16 +0800

    Add Migration - add user table - add picture table - add bookmark table

---
+++ database/migrations/2021_06_21_160557_create_bookmarks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookmarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookmarks', function (Blueprint $table) {
+            $table->id('book_id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('pict_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('bookmarks', function (Blueprint $table) {
+            $table->foreign('user_id')->references('user_id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('pict_id')->references('pict_id')->on('pictures')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookmarks');
+    }
+}
